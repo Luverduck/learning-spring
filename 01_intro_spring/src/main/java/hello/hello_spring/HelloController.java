58e32b75814a16e69f42b1f77e9fd3727add805c
Luverduck <hyekko92@gmail.com>
2024-06-02 04:18:33 +0900

    02-01. 정적 콘텐츠

--- 01_intro_spring/src/main/java/hello/hello_spring/HelloController.java
+++ 01_intro_spring/src/main/java/hello/hello_spring/HelloController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
     // [01-03] START
-    // 01. 웹 브라우저에서 http://localhost:8080/hello로 보낸 요청한다.
-    // 02. /hello인 요청을 HelloController의 hello 메소드로 연결한다.
+    // 01. 웹 브라우저에서 'http://localhost:8080/hello'로 요청을 보낸다.
+    // 02. '/hello'인 요청을 HelloController의 hello 메소드로 연결한다.
     // 03. hello 메소드에서 Model에 key-value 형태의 속성 {"data" : "Hello!!!"}을 추가한다.
     // 04. hello 메소드 실행 후 View 이름인 "hello"를 반환한다.
     // 05. ViewResolver는 templates 폴더 하위에서 이름이 "hello"인 템플릿을 찾는다.
