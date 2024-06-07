b76080b999b7dc62b725202346fe4e3de45c0979
Luverduck <hyekko92@gmail.com>
2024-06-07 16:51:20 +0900

    [04-01] 컴포넌트 스캔과 자동 의존관계 설정

--- 01_intro_spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ 01_intro_spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -19,6 +19,6 @@ public class MemberController {
     // MemberController와 MemberService 사이의 의존관계 생성
     // 1. MemberController형 객체를 생성하기 위해서는 생성자를 호출해야 한다.
     // 2. 스프링 컨테이너에서 전달받은 MemberService형 객체를 인수로 생성자를 호출한다.
-    // 3. MemberController(this)형 객체의 멤버인 memberRepository에 스프링 컨테이너에서 전달받은 MemberRepository형 객체를 대입한다.
+    // 3. MemberController(this)형 객체의 멤버인 memberService에 스프링 컨테이너에서 전달받은 MemberService형 객체를 대입한다.
 }
 // [04-01] END
