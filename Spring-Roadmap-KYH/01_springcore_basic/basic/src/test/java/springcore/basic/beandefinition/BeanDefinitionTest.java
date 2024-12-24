9394239f9e3e9d385f1fd513fd5109341eff7a3d
Luverduck <hyekko92@gmail.com>
2024-12-25 03:13:56 +0900

    05.08. 스프링 빈 설정 메타 정보 - BeanDefinition

---
+++ Spring-Roadmap-KYH/01_springcore_basic/basic/src/test/java/springcore/basic/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,25 @@
+package springcore.basic.beandefinition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    //AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+    // ApplicationContext 타입으로 선언하면 getDefinition() 사용 불가
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for(String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if(beanDefinition.getRole() != BeanDefinition.ROLE_APPLICATION) continue;
+            System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+        }
+    }
+
+}
