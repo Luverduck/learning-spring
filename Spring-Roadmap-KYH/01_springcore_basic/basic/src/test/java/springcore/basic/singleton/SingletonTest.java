173c997188b29fe6812d70efba625c271985df53
Luverduck <hyekko92@gmail.com>
2024-12-25 21:46:24 +0900

    06.03. 싱글톤 컨테이너

--- Spring-Roadmap-KYH/01_springcore_basic/basic/src/test/java/springcore/basic/singleton/SingletonTest.java
+++ Spring-Roadmap-KYH/01_springcore_basic/basic/src/test/java/springcore/basic/singleton/SingletonTest.java
@@ -2,6 +2,7 @@ package springcore.basic.singleton;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springcore.basic.AppConfig;
 import springcore.basic.member.MemberService;
 
@@ -44,6 +45,25 @@ public class SingletonTest {
         // [참고]
         // isSameAs() : 두 객체의 참조가 같은지 비교 ( == )
         // isEqualTo() : 두 객체의 값이 같은지 비교 ( equals() )
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        // 스프링 컨테이너 생성
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 클라이언트의 요청을 처리하기 위해 컨테이너에서 MemberService 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        // 클라이언트의 요청을 처리하기 위해 컨테이너에서 MemberService 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 두 객체의 참조값이 같은 것을 확인 => 매 번 요청을 처리하기 위해 기존에 생성했던 MemberService 객체를 반환한다.
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService1 = " + memberService2);
+
+        // 테스트 성공 조건은 두 객체의 참조가 같은 경우이다.
+        assertThat(memberService1).isSameAs(memberService2);
     }
 
 }
