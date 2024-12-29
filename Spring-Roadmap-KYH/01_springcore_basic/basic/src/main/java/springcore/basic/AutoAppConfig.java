520374aadef6bc267fae96d77c986cb6c4fdb5b6
Luverduck <hyekko92@gmail.com>
2024-12-29 21:34:36 +0900

    07.04. 중복 등록과 충돌 - 자동 등록 방식과 수동 등록 방식의 혼용에서 빈 중복 등록 테스트

--- Spring-Roadmap-KYH/01_springcore_basic/basic/src/main/java/springcore/basic/AutoAppConfig.java
+++ Spring-Roadmap-KYH/01_springcore_basic/basic/src/main/java/springcore/basic/AutoAppConfig.java
@@ -1,8 +1,11 @@
 package springcore.basic;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import springcore.basic.member.MemberRepository;
+import springcore.basic.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -12,5 +15,11 @@ import org.springframework.context.annotation.FilterType;
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 빈 이름 : memoryMemberRepository
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
