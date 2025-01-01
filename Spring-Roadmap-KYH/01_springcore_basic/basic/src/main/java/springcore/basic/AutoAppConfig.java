3f51fbe7a6892ea48ea559b219e6c9183495de26
Luverduck <hyekko92@gmail.com>
2025-01-01 18:14:43 +0900

    07.04. 중복 등록과 충돌 - AutoAppConfig의 수동 등록 빈 주석 처리

--- Spring-Roadmap-KYH/01_springcore_basic/basic/src/main/java/springcore/basic/AutoAppConfig.java
+++ Spring-Roadmap-KYH/01_springcore_basic/basic/src/main/java/springcore/basic/AutoAppConfig.java
@@ -1,11 +1,8 @@
 package springcore.basic;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import springcore.basic.member.MemberRepository;
-import springcore.basic.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -15,11 +12,11 @@ import springcore.basic.member.MemoryMemberRepository;
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    /*
     // 빈 이름 : memoryMemberRepository
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    */
 }
