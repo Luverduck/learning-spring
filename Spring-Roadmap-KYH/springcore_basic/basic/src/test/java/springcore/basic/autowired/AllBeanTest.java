b258a70dca20242a8b3fbd5a0d24e198a9a43903
Luverduck <hyekko92@gmail.com>
2025-01-14 01:45:54 +0900

    08.08. 특정 타입의 모든 빈을 자동 의존 관계 주입 - 특정 타입의 모든 빈을 자동 의존 관계 주입 테스트

---
+++ Spring-Roadmap-KYH/springcore_basic/basic/src/test/java/springcore/basic/autowired/AllBeanTest.java
@@ -0,0 +1,37 @@
+package springcore.basic.autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springcore.basic.AutoAppConfig;
+import springcore.basic.discount.DiscountPolicy;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        // 스프링 컨테이너 생성 후 AutoAppConfig와 DiscountService를 컨테이너에 등록
+        // 컴포넌트 스캔을 통해 빈 등록 후 DiscountService에 자동 의존 관계 주입
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+    }
+
+    static class DiscountService {
+        // DiscountPolicy 타입 빈의 이름과 객체를 Map으로 반환
+        private final Map<String, DiscountPolicy> policyMap;
+        // DiscountPolicy 타입 빈 객체를 List로 반환
+        private final List<DiscountPolicy> policies;
+        // 생성자 주입
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+    }
+
+}
