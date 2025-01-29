3856b35b3539fff7157c1bd486c1d298b64cc82f
Luverduck <hyekko92@gmail.com>
2025-01-29 13:43:33 +0900

    02.05. 데이터베이스 연결

--- Spring-Roadmap-KYH/spring-db-basic/jdbc/src/main/java/springdb/jdbc/connection/DBConnectionUtil.java
+++ Spring-Roadmap-KYH/spring-db-basic/jdbc/src/main/java/springdb/jdbc/connection/DBConnectionUtil.java
@@ -13,7 +13,7 @@ public class DBConnectionUtil {
 
     public static Connection getConnection() {
         try {
-            // DB 접속 정보를 통해 커넥션 생성 후 반환
+            // DriverManager에서 DB 접속 정보를 통해 커넥션 생성 후 반환
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             log.info("get Connection={}, class={}", connection, connection.getClass());
             return connection;
