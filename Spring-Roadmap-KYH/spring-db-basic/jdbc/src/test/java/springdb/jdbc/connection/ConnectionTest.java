92c7ac86c1c1697c7c4700b044ec70b7e7cffb79
Luverduck <hyekko92@gmail.com>
2025-01-30 20:19:55 +0900

    03.04. DataSource 예제2 - 커넥션 풀

--- Spring-Roadmap-KYH/spring-db-basic/jdbc/src/test/java/springdb/jdbc/connection/ConnectionTest.java
+++ Spring-Roadmap-KYH/spring-db-basic/jdbc/src/test/java/springdb/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package springdb.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -25,10 +26,27 @@ public class ConnectionTest {
 
     // DriverManagerDataSource를 사용하여 DB 커넥션 얻기
     @Test
-    void driverManagerDataSource() throws SQLException {
-        // 설정
+    void dataSourceDriverManager() throws SQLException {
+        // DriverManagerDataSource 설정
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        // DB 커넥션 얻기
         useDataSource(dataSource);
+    }
+
+    // HikariCP를 사용하여 DB 커넥션 얻기
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // HikariDataSource 설정
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10); // 커넥션 풀 사이즈
+        dataSource.setPoolName("MyConnectionPool"); // 커넥션 풀 이름
+        // DB 커넥션 얻기
+        useDataSource(dataSource);
+        // 커넥션 풀에서 커넥션이 생성되는 로그를 출력하기 위함
+        Thread.sleep(1000);
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
