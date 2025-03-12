b7704f4eb357ca47f9dc0eb95200d2ced324ef6a
Luverduck <hyekko92@gmail.com>
2025-03-12 22:30:18 +0900

    06.06. 스프링 MVC - 실용적인 방식 (Model 전달 및 viewName 반환)

---
+++ spring-roadmap-kyh/spring-mvc-basic/servlet/src/main/java/springmvc/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,51 @@
+package springmvc.servlet.web.springmvc.v3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import springmvc.servlet.domain.member.Member;
+import springmvc.servlet.domain.member.MemberRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members/")
+public class SpringMemberControllerV3 {
+
+    // 회원 저장소
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // 회원 등록 폼 요청 처리
+    @RequestMapping("/new-form")
+    public String newForm() {
+        // 뷰의 이름 반환
+        return "new-form";
+    }
+
+    // 회원 등록 요청 처리
+    @RequestMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model) {
+        // 비즈니스 로직 처리
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        // 비즈니스 로직의 결과를 모델에 저장
+        model.addAttribute("member", member);
+        // 뷰의 이름 반환
+        return "save-result";
+    }
+
+    // 회원 조회 요청 처리
+    @RequestMapping()
+    public String members(Model model) {
+        // 비즈니스 로직 실행
+        List<Member> members = memberRepository.findAll();
+        // 비즈니스 로직의 결과를 모델에 저장
+        model.addAttribute("members", members);
+        // 뷰의 이름 반환
+        return "members";
+    }
+}
