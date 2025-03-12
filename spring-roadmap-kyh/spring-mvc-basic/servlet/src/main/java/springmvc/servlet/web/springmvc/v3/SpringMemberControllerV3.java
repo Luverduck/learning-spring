d8a579447c26a955b486e84408bc1b8e83ebf850
Luverduck <hyekko92@gmail.com>
2025-03-12 22:32:47 +0900

    06.06. 스프링 MVC - 실용적인 방식 (HTTP 메소드 어노테이션 사용)

--- spring-roadmap-kyh/spring-mvc-basic/servlet/src/main/java/springmvc/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ spring-roadmap-kyh/spring-mvc-basic/servlet/src/main/java/springmvc/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -2,9 +2,7 @@ package springmvc.servlet.web.springmvc.v3;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import springmvc.servlet.domain.member.Member;
 import springmvc.servlet.domain.member.MemberRepository;
 
@@ -18,14 +16,14 @@ public class SpringMemberControllerV3 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     // 회원 등록 폼 요청 처리
-    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         // 뷰의 이름 반환
         return "new-form";
     }
 
     // 회원 등록 요청 처리
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
@@ -40,7 +38,7 @@ public class SpringMemberControllerV3 {
     }
 
     // 회원 조회 요청 처리
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping()
     public String members(Model model) {
         // 비즈니스 로직 실행
         List<Member> members = memberRepository.findAll();
