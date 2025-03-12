b34dbb73ff9e9a4926a5601b7ab547a5c26a31e2
Luverduck <hyekko92@gmail.com>
2025-03-12 20:54:16 +0900

    주석 추가

--- spring-roadmap-kyh/spring-mvc-basic/servlet/src/main/java/springmvc/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ spring-roadmap-kyh/spring-mvc-basic/servlet/src/main/java/springmvc/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SpringMemberFormControllerV1 {
 
+    // HTTP 요청 처리 메소드 구현 - 회원 등록 폼 요청 처리
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process() {
         // 모델과 뷰의 이름이 저장된 ModelAndView 객체 반환
