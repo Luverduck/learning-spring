a100008fc4e047c79b4c52d455893d7984c6ea96
Luverduck <hyekko92@gmail.com>
2025-04-06 02:59:19 +0900

    08.06. 상품 상세

--- spring-roadmap-kyh/spring-mvc-basic/itemservice/src/main/java/springmvc/itemservice/web/basic/BasicController.java
+++ spring-roadmap-kyh/spring-mvc-basic/itemservice/src/main/java/springmvc/itemservice/web/basic/BasicController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springmvc.itemservice.domain.item.Item;
 import springmvc.itemservice.domain.item.ItemRepository;
@@ -16,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor // final이나 @NonNull이 적용된 필드만 포함된 생성자를 자동으로 생성
 public class BasicController {
 
+    // 상품 저장소
     private final ItemRepository itemRepository;
 
     /**
@@ -27,10 +29,28 @@ public class BasicController {
         itemRepository.save(new Item("itemB", 20000, 20));
     }
 
+    /**
+     * 상품 목록
+     * @param model
+     * @return
+     */
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    /**
+     * 상품 상세
+     * @param itemId
+     * @param model
+     * @return
+     */
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 }
