f2a551bb86418cabf6d0d3920b097d64a4a14e9d
Luverduck <hyekko92@gmail.com>
2025-04-07 21:38:17 +0900

    08.07. 상품 등록 - 상품 등록 폼

--- spring-roadmap-kyh/spring-mvc-basic/itemservice/src/main/java/springmvc/itemservice/web/basic/BasicController.java
+++ spring-roadmap-kyh/spring-mvc-basic/itemservice/src/main/java/springmvc/itemservice/web/basic/BasicController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springmvc.itemservice.domain.item.Item;
 import springmvc.itemservice.domain.item.ItemRepository;
@@ -53,4 +54,19 @@ public class BasicController {
         model.addAttribute("item", item);
         return "basic/item";
     }
+
+    /**
+     * 상품 등록 폼
+     * @return
+     */
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "XXX";
+    }
+
 }
