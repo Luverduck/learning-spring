a1060e04128d8e5bc51ebb136d26238933707a8c
Luverduck <hyekko92@gmail.com>
2025-05-05 22:11:25 +0900

    07.01. 검사 예외와 인터페이스

--- spring-roadmap-kyh/spring-db-basic/jdbc/src/main/java/springdb/jdbc/repository/MemberRepositoryEX.java
+++ spring-roadmap-kyh/spring-db-basic/jdbc/src/main/java/springdb/jdbc/repository/MemberRepositoryEX.java
@@ -2,17 +2,19 @@ package springdb.jdbc.repository;
 
 import springdb.jdbc.domain.Member;
 
+import java.sql.SQLException;
+
 public interface MemberRepositoryEX {
 
     // 회원 정보 저장
-    Member save(Member member);
+    Member save(Member member) throws SQLException;
 
     // 회원 정보 조회
-    Member findById(String memberId);
+    Member findById(String memberId) throws SQLException;
 
     // 회원 정보 수정
-    void update(String memberId, int money);
+    void update(String memberId, int money) throws SQLException;
 
     // 회원 정보 삭제
-    void delete(String memberId);
+    void delete(String memberId) throws SQLException;
 }
