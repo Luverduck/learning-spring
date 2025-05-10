3524205dce3ad8011c42e00f9e485470be300bab
Luverduck <hyekko92@gmail.com>
2025-05-10 22:57:30 +0900

    02.04. 변수 - SpringEL

--- spring-roadmap-kyh/spring-mvc-advanced/thymeleaf/src/main/java/springmvc/thymeleaf/basic/BasicController.java
+++ spring-roadmap-kyh/spring-mvc-advanced/thymeleaf/src/main/java/springmvc/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package springmvc.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -14,6 +20,7 @@ public class BasicController {
     public String textBasic(Model model) {
         // Model에 데이터 추가
         model.addAttribute("data", "Hello, Spring!");
+        // 뷰의 이름 반환
         return "basic/text-basic";
     }
 
@@ -22,6 +29,39 @@ public class BasicController {
     public String textUnescaped(Model model) {
         // Model에 사용이 예약된 특수문자가 포함된 데이터 추가
         model.addAttribute("data", "Hello, <b>Spring!</b>");
+        // 뷰의 이름 반환
         return "basic/text-unescaped";
+    }
+
+    // 변수 표현식
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        // 반환할 데이터 생성
+        User userA = new User("userA", 10);
+        User userB = new User("userA", 20);
+        // 반환할 데이터를 List 형태로 생성
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+        // 반환할 데이터를 Map 형태로 생성
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+        // Model에 단일 객체, List, Map 형태의 데이터 추가
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+        // 뷰의 이름 반환
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
